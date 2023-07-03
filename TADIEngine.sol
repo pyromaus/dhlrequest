8d2c19202192f5224aafc13146a19bb5a1dbc982
pyromaus <57569562+pyromaus@users.noreply.github.com>
2023-07-03 18:55:57 +0200

    Update TADIEngine.sol

--- TADIEngine.sol
+++ TADIEngine.sol
@@ -1,17 +1,40 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+/**
+ * @title TADIEngine 
+ * @author Serge Fotiev
+ * @notice Tracking and Delay Insurance (TADI). This parametric insurance 
+ * contract will allow a shipping client to purchase delay protection for their 
+ * shipping container. Access to Etherisc risk pools not yet implemented.
+ * Also makes use of Chainlink Functions (beta) to call the
+ * DHL unified shipment tracking API with the Chainlink DON to fetch the 
+ * current location and timestamp of a given tracking number.
+ * @dev Chainlink Functions (beta) - Mumbai
+ */
+
 import {Functions, FunctionsClient} from "./dev/functions/FunctionsClient.sol";
 import "./strings.sol";
+
+error TADIEngine__NotOwner();
+error TADIEngine__InvalidAmountSent(uint amount);
 
 contract TADIEngine is FunctionsClient {
   using Functions for Functions.Request;
   using strings for *;
 
+  /* structs */
+
+  /* when the contract refers to the shipper, we mean the 
+  owner who is managing/tracking/awaiting their shipments */
+
   struct Shipper {
     uint shipperID;
-    address shipperAddy;
+    address shipperAddress;
   }
+
+  /* container refers to the shipment owned by the shipper */ 
+
   struct Container {
     uint containerID;
     uint shipperID;
@@ -26,87 +49,109 @@ contract TADIEngine is FunctionsClient {
     bool delayProtection;
   }
 
-  mapping(address => Shipper) public shippers;
-  mapping(uint => address) public shipperIdToAddress;
-  uint[] public shipperIndex;
-  mapping(address => mapping(uint => Container)) public containers;
-  mapping(uint => address) public containerIdToOwner;
-  mapping(address => uint[]) public containerIndex;
-  mapping(string => uint) public trackingNoToContainerId;
-  mapping(uint => string) public containerIdToTrackingNo;
+  /* state variables */ 
 
-  address public owner;
-  bool public active;
+  mapping(address => Shipper) private s_shippers;
+  mapping(uint => address) private s_shipperIdToAddress;
+  uint[] private s_shipperIndex;
+  mapping(address => mapping(uint => Container)) private s_containers;
+  mapping(uint => address) private s_containerIdToOwner;
+  mapping(address => uint[]) private s_containerIndex;
+  mapping(uint => string) private s_containerIdToTrackingNo;
 
+  address private immutable i_owner;
+  string[] private s_latestTrackingData;
+  uint private s_containerIdToTrack;
+  uint private s_premium = 20000000000000000; // 0.02 MATIC
+  uint private s_payout = 200000000000000000; // 0.20 MATIC
+  uint private SEQ_shipperID = 0;
+  uint private SEQ_containerID = 0;
+
+  // Chainlink Functions variables
   bytes32 public latestRequestId;
   bytes public latestResponse;
   bytes public latestError;
-  address public functions_oracle;
-  string[] public latestTrackingData;
 
-  uint public premium = 20000000000000000;
-  uint public payout = 200000000000000000;
-  uint public SEQ_shipperID = 0;
-  uint public SEQ_containerID = 0;
+  /* events */
 
   event OCRResponse(bytes32 indexed requestId, bytes result, bytes err);
-
   event Received(address _sender, uint _value);
+  event ShipperCreated(address indexed _shipper);
+  event ContainerCreated(address indexed _shipper, uint indexed _containerID);
 
   modifier OnlyOwner() {
-    if (msg.sender != owner) {
+    if (msg.sender != i_owner) {
       revert TADIEngine__NotOwner();
     }
     _;
   }
-  modifier ContractActive() {
-    if (!active) {
-      revert TADIEngine__NotActive();
-    }
-    _;
+
+  constructor (address oracle) payable FunctionsClient(oracle) {
+    i_owner = msg.sender;
   }
 
-  error TADIEngine__NotOwner();
-  error TADIEngine__NotActive();
-  error TADIEngine__InvalidAmountSent(uint amount);
+  receive() external payable {
+    emit Received(msg.sender, msg.value);
+  }
 
-  constructor(address oracle) payable FunctionsClient(oracle) {
-    owner = msg.sender;
-    active = true;
-    functions_oracle = oracle;
+  /**@dev An interaction with the Etherisc risk pools will
+   * be implemented in this function. The protection buyer
+   * will pay a premium for risk pool access. If shipment is
+   * late, the smart contract acts accordingly and the buyer
+   * is paid immediately.
+   */
+  function purchaseDelayProtection(uint _containerID) external payable {
+    if (msg.value < s_premium) {
+      revert TADIEngine__InvalidAmountSent(msg.value);
+    }
+    s_containers[getContainerOwner(_containerID)][_containerID].delayProtection = true;
+  }
+
+  /** @dev Here we set the shipment's due date to yesterday,
+   * to simulate a delay and warrant a payout.
+   */
+  function simulateDelay(uint _containerID) external {
+    s_containers[getContainerOwner(_containerID)][_containerID].dueDate = block.timestamp - 86400;
+  }
+
+  /** @dev If current block time is past the due date, and 
+   * the buyer has delay protection purchased, 
+   * they are paid immediately by this contract.
+   */
+  function checkForDelay(uint _containerID) external {
+    uint dueDatez = s_containers[getContainerOwner(_containerID)][_containerID].dueDate;
+    if (block.timestamp > dueDatez && s_containers[getContainerOwner(_containerID)][_containerID].delayProtection) {
+      s_containers[getContainerOwner(_containerID)][_containerID].delayProtection = false;
+      insurancePayout(_containerID);
+    }
   }
 
   function newShipper(address _shipper) public returns (uint) {
-    address shipperAddy;
-    if (msg.sender == owner) {
-      shipperAddy = _shipper;
-    } else {
-      shipperAddy = msg.sender;
-    }
     SEQ_shipperID++;
-    shippers[shipperAddy] = Shipper(SEQ_shipperID, shipperAddy);
-    shipperIdToAddress[SEQ_shipperID] = shipperAddy;
-    shipperIndex.push(SEQ_shipperID);
+    s_shippers[_shipper] = Shipper(SEQ_shipperID, _shipper);
+    s_shipperIdToAddress[SEQ_shipperID] = _shipper;
+    s_shipperIndex.push(SEQ_shipperID);
+    emit ShipperCreated(_shipper);
     return SEQ_shipperID;
   }
 
+  /**@dev adds a new shipment with data on its owner,
+   * ID, weight, origin and last location with timestamps,
+   * tracking number and due date.
+   * Delay protection is not included and may be 
+   * purchased by calling purchaseDelayProtection()
+   */
   function addContainer(
     address _shipper,
     string memory _origin,
     uint _gWeight,
     string memory _trackingNumber,
     uint _dueDate
-  ) public payable returns (uint) {
-    address shipperAddy;
-    if (msg.sender == owner) {
-      shipperAddy = _shipper;
-    } else {
-      shipperAddy = msg.sender;
-    }
+  ) public returns (uint) {
     SEQ_containerID++;
-    containers[shipperAddy][SEQ_containerID] = Container(
+    s_containers[_shipper][SEQ_containerID] = Container(
       SEQ_containerID,
-      shippers[shipperAddy].shipperID,
+      s_shippers[_shipper].shipperID,
       _gWeight,
       _origin,
       block.timestamp,
@@ -117,97 +162,41 @@ contract TADIEngine is FunctionsClient {
       _dueDate,
       false
     );
-    containerIdToOwner[SEQ_containerID] = shipperAddy;
-    containerIndex[shipperAddy].push(SEQ_containerID);
-    containerIdToTrackingNo[SEQ_containerID] = _trackingNumber;
+    s_containerIdToOwner[SEQ_containerID] = _shipper;
+    s_containerIndex[_shipper].push(SEQ_containerID);
+    s_containerIdToTrackingNo[SEQ_containerID] = _trackingNumber;
+    emit ContainerCreated(_shipper, SEQ_containerID);
     return SEQ_containerID;
   }
 
-  function getTrackingNumber(uint _containerID) public view returns (string memory) {
-    return containerIdToTrackingNo[_containerID];
-  }
-
-  function getContainerId(string memory _trackingNumber) public view returns (uint) {
-    return trackingNoToContainerId[_trackingNumber];
-  }
-
-  function purchaseDelayProtection(uint _containerID) public payable {
-    if (msg.value < premium) {
-      revert TADIEngine__InvalidAmountSent(msg.value);
-    }
-    containers[getContainerOwner(_containerID)][_containerID].delayProtection = true;
-  }
-
-  receive() external payable {
-    emit Received(msg.sender, msg.value);
-  }
-
-  function simulateDelay(uint _containerID) public {
-    containers[getContainerOwner(_containerID)][_containerID].dueDate = block.timestamp - 86400;
-  }
-
-  function checkForDelay(uint _containerID) public {
-    uint dueDatez = containers[getContainerOwner(_containerID)][_containerID].dueDate;
-    if (block.timestamp > dueDatez && containers[getContainerOwner(_containerID)][_containerID].delayProtection) {
-      insurancePayout(_containerID);
-      containers[getContainerOwner(_containerID)][_containerID].delayProtection = false;
-    }
-  }
-
-  function insurancePayout(uint _containerID) internal ContractActive {
-    address ownah = getContainerOwner(_containerID);
-    payable(ownah).transfer(payout);
-  }
-
-  function modifyGrossWeight(address _shipper, uint _containerID, uint newWeight) public ContractActive returns (uint) {
-    address shipperAddy;
-    if (msg.sender == owner) {
-      shipperAddy = _shipper;
-    } else {
-      shipperAddy = msg.sender;
-    }
-    containers[shipperAddy][_containerID].grossWeight = newWeight;
-    return newWeight;
-  }
-
-  function getLatestLocation(uint _containerID) public view OnlyOwner returns (string memory) {
-    return containers[getContainerOwner(_containerID)][_containerID].latestLoc;
-  }
-
-  function getLatestTimestamp(uint _containerID) public view OnlyOwner returns (uint) {
-    return containers[getContainerOwner(_containerID)][_containerID].latestTimestamp;
-  }
-
-  function concludeContainer(
-    // address _shipper,
-    uint _containerID
-  ) public OnlyOwner ContractActive returns (bool) {
-    containers[getContainerOwner(_containerID)][_containerID].active = false;
-    return true;
-  }
-
-  function reactivateContainer(
-    // address _shipper,
+  function concludeShipment(
     uint _containerID
   ) public OnlyOwner returns (bool) {
-    containers[getContainerOwner(_containerID)][_containerID].active = true;
+    s_containers[getContainerOwner(_containerID)][_containerID].active = false;
     return true;
   }
 
-  function getContainerOwner(uint _containerID) public view returns (address) {
-    return containerIdToOwner[_containerID];
+  function activateShipment(
+    uint _containerID
+  ) public OnlyOwner returns (bool) {
+    s_containers[getContainerOwner(_containerID)][_containerID].active = true;
+    return true;
   }
 
-  function getShipperAddressById(uint _shipperId) public view returns (address) {
-    return shipperIdToAddress[_shipperId];
-  }
-
-  function executeRequest(
+  /**@dev Builds a Chainlink Functions request;
+   * Chainlink executes our DHL shipment
+   * tracking API request on 10 different 
+   * decentralised oracle nodes and aggregates
+   * a single response.
+   * Currently only callable in Javascript
+   */
+  function requestShipmentTracking(
     string calldata source,
     bytes calldata secrets,
     string[] calldata args,
     uint64 subscriptionId,
-    uint32 gasLimit
+    uint32 gasLimit,
+    uint _containerID
   ) public OnlyOwner returns (bytes32) {
     Functions.Request memory req;
     req.initializeRequest(Functions.Location.Inline, Functions.CodeLanguage.JavaScript, source);
@@ -218,33 +207,43 @@ contract TADIEngine is FunctionsClient {
 
     bytes32 assignedReqID = sendRequest(req, subscriptionId, gasLimit);
     latestRequestId = assignedReqID;
+    s_containerIdToTrack = _containerID;
     return assignedReqID;
   }
 
+  /**@dev Chainlink Functions callback
+   * Called by Chainlink DON when they have our 
+   * tracking data response ready
+   */
   function fulfillRequest(bytes32 requestId, bytes memory response, bytes memory err) internal override {
     latestResponse = response;
     latestError = err;
-    
 
     bool nilErr = (err.length == 0);
     if (nilErr) {
-      string memory snapshot = string(response);
-      latestTrackingData = smt(snapshot);
+      string memory trackingInfo = string(response);
+      s_latestTrackingData = smt(trackingInfo);
     }
+    writeTrackingResponseToContainer(s_containerIdToTrack);
     emit OCRResponse(requestId, response, err);
   }
 
-  function trackingUpdater(uint _containerId) public OnlyOwner {
-    containers[getContainerOwner(_containerId)][_containerId].latestLoc = latestTrackingData[0];
-    containers[getContainerOwner(_containerId)][_containerId].latestTimestamp = st2num(latestTrackingData[1]);
+  function writeTrackingResponseToContainer(uint _containerId) internal {
+    s_containers[getContainerOwner(_containerId)][_containerId].latestLoc = s_latestTrackingData[0];
+    s_containers[getContainerOwner(_containerId)][_containerId].latestTimestamp = st2num(s_latestTrackingData[1]);
   }
 
-  function ltrTester(string memory _loc, string memory _time) public OnlyOwner {
-    latestTrackingData[0] = _loc;
-    latestTrackingData[1] = _time;
+  function insurancePayout(uint _containerID) internal {
+    address owner = getContainerOwner(_containerID);
+    payable(owner).transfer(s_payout);
   }
 
-  function smt(string memory _snapshot) public pure returns (string[] memory) {
+  /**@dev Response received by callback is the
+   * shipment location and timestamp in one string
+   * for example "Germany-1683486969"
+   * This function splits this single string in two
+   */
+  function smt(string memory _snapshot) internal pure returns (string[] memory) {
     strings.slice memory s = _snapshot.toSlice();
     strings.slice memory delim = "-".toSlice();
     string[] memory parts = new string[](s.count(delim) + 1);
@@ -254,7 +253,12 @@ contract TADIEngine is FunctionsClient {
     return parts;
   }
 
-  function st2num(string memory numString) public pure returns (uint) {
+  /**@dev Once the location and timestamp are seperated, 
+   * this function will convert the timestamp from
+   * a string to a uint256
+   * "1683486969" --> 1683486969
+   */
+  function st2num(string memory numString) internal pure returns (uint) {
     uint val = 0;
     bytes memory stringBytes = bytes(numString);
     for (uint i = 0; i < stringBytes.length; i++) {
@@ -267,5 +271,30 @@ contract TADIEngine is FunctionsClient {
     }
     return val;
   }
+
+  /* Getter functions */
+
+  function getContainerOwner(uint _containerID) public view returns (address) {
+    return s_containerIdToOwner[_containerID];
+  }
+
+  function getShipperAddressById(uint _shipperId) public view returns (address) {
+    return s_shipperIdToAddress[_shipperId];
+  }
+
+  function getLatestLocation(uint _containerID) public view returns (string memory) {
+    return s_containers[getContainerOwner(_containerID)][_containerID].latestLoc;
+  }
+
+  function getLatestTimestamp(uint _containerID) public view returns (uint) {
+    return s_containers[getContainerOwner(_containerID)][_containerID].latestTimestamp;
+  }
+
+  function getTrackingNumber(uint _containerID) public view returns (string memory) {
+    return s_containerIdToTrackingNo[_containerID];
+  }
+
+  function getContainerIds(address _shipper) public view returns (uint[] memory) {
+    return s_containerIndex[_shipper];
+  }
 }
-// Germany-169872345
